6460936856acbad9fd73b5441a714ad1fbf236cc
teraoka <92132913+Terettetererere@users.noreply.github.com>
2023-09-21 07:34:10 +0000

    change

--- utils.py
+++ utils.py
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import inspect
-import textwrap
+# import inspect
+# import textwrap
 
-import streamlit as st
+# import streamlit as st
 
 
 # def show_code(demo):
